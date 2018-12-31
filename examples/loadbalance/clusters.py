6ce58a426e3ca632c4e3bd63363b656271c5bea1
natj <nattila.joonas@gmail.com>
2018-12-31 14:26:12 +0200

    clustering methods

--- examples/loadbalance/clusters.py
+++ examples/loadbalance/clusters.py
@@ -10,7 +10,8 @@ palette = pal.wesanderson.Moonrise1_5.mpl_colormap
 
 
 class Conf:
-    outdir = "out"
+    #outdir = "out"
+    outdir = "out4_30x30"
 
 
 def reduce_image(img, val):
@@ -84,6 +85,22 @@ def analyze_contours(cnts, hierarchy):
     res['N'] = N
     print("Number of clusters: {}".format(N))
 
+    res['areas'] = []
+    res['arcs'] = []
+    for cnt in cnts:
+        res['areas'].append( cv2.contourArea(cnt) )
+        res['arcs'].append(  cv2.arcLength(cnt, True) )
+
+    res['area_mean'] = np.mean(res['areas'])
+    res['area_min']  = np.min( res['areas'])
+    res['area_max']  = np.max( res['areas'])
+    print("area mean={} min={} max={}".format(res['area_mean'], res['area_min'], res['area_max']))
+
+    res['arc_mean'] = np.mean(res['arcs'])
+    res['arc_min']  = np.min( res['arcs'])
+    res['arc_max']  = np.max( res['arcs'])
+    print("arc mean={} min={} max={}".format(res['arc_mean'], res['arc_min'], res['arc_max']))
+
     return res
 
 
@@ -91,18 +108,21 @@ if __name__ == "__main__":
 
     # set up plotting and figure
     #plt.fig = plt.figure(1, figsize=(3.4,2.5))
-    fig = plt.figure(figsize=(3.54, 2.2)) #single column fig
+    fig = plt.figure(figsize=(3.54, 4.5)) #single column fig
     #fig = plt.figure(figsize=(7.48, 4.0))  #two column figure
 
     plt.rc('font', family='serif', size=8)
     plt.rc('xtick')
     plt.rc('ytick')
     
-    gs = plt.GridSpec(1, 1)
-    gs.update(hspace = 0.5)
+    gs = plt.GridSpec(4, 1)
+    gs.update(hspace = 0.0)
     
     axs = []
     axs.append( plt.subplot(gs[0]) )
+    axs.append( plt.subplot(gs[1]) )
+    axs.append( plt.subplot(gs[2]) )
+    axs.append( plt.subplot(gs[3]) )
 
     for ax in axs:
         ax.minorticks_on()
@@ -111,7 +131,9 @@ if __name__ == "__main__":
 
     ##################################################
     cols = ['k','b','r','g']
-    for ir in range(2):
+    for ir in range(4):
+        col = cols[ir]
+
         rank = str(ir)
         f5 = h5py.File(conf.outdir+"/run-"+rank+".h5", "r")
 
@@ -120,6 +142,16 @@ if __name__ == "__main__":
         locs = f5['locals']
         
         imgs = f5['grid'][:,:,:]
+
+        N = []
+
+        arc_mean = []
+        arc_min  = []
+        arc_max  = []
+
+        area_mean = []
+        area_min  = []
+        area_max  = []
 
         nx, ny, nt = np.shape(imgs)
         print("image size nx {} ny {} nt {}".format(nx, ny, nt))
@@ -132,13 +164,52 @@ if __name__ == "__main__":
             cnts, hier = compute_clusters(img)
             data = analyze_contours(cnts, hier)
 
-            imshow(axs[0], img)
+            #imshow(axs[0], img, lap)
+
+            N.append(data['N'])
+
+            arc_mean.append(data['arc_mean'])
+            arc_min.append(data['arc_min'])
+            arc_max.append(data['arc_max'])
+
+            area_mean.append(data['area_mean'])
+            area_min.append(data['area_min'])
+            area_max.append(data['area_max'])
+
+        #axs[0].plot(locs, linestyle='solid' , color=cols[ir] )
+        #axs[0].plot(virs, linestyle='solid', color=cols[ir] )
+        axs[0].plot(np.array(virs)/np.array(locs), linestyle='solid', color=cols[ir] )
+
+        axs[1].plot(N, color=col)
+
+        axs[2].plot(arc_mean, linestyle='solid', color=col)
+        axs[2].fill_between(np.arange(len(arc_mean)), 
+                arc_min, arc_max,
+                color=col, alpha=0.5, edgecolor=None)
+
+        axs[3].plot(area_mean, linestyle='solid', color=col)
+        axs[3].fill_between(np.arange(len(area_mean)), 
+                area_min, area_max,
+                color=col, alpha=0.5, edgecolor=None)
 
 
+    axs[0].set_ylabel(r'$N_{\mathrm{vir}}/N_{\mathrm{loc}}$')
 
-    #axs[0].set_xlabel(r'Step $n$')
-    #axs[0].set_yscale('log')
+    axs[1].set_ylabel(r'$k$-clusters')
+    axs[2].set_ylabel(r'Arc length')
+    axs[3].set_ylabel(r'Area')
 
-    plt.subplots_adjust(left=0.18, bottom=0.18, right=0.98, top=0.98, wspace=0.0, hspace=0.0)
+    #for ax in axs:
+    #    ax.set_yscale('log')
+    #    #ax.set_xscale('log')
+
+    axs[1].set_yscale('log')
+    axs[2].set_yscale('log')
+    axs[3].set_yscale('log')
+
+    axs[3].set_xlabel(r'Step $n$')
+
+
+    plt.subplots_adjust(left=0.18, bottom=0.12, right=0.98, top=0.98, wspace=0.0, hspace=0.0)
     plt.savefig('clusters.pdf')
 
